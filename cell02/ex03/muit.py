0dd636e5f916eb3ba5cca281239e1b3028ed56b1
psamphao <68030153@kmitl.ac.th>
2025-06-17 07:41:29 +0000

    cell00/ex01

---
+++ cell02/ex03/muit.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter the first n"))
+num2 = int(input("Enter the second"))
+print(f"nu{num1})x{num2}={num1*num2}")
+if num1 * num2 > 0:
+    print("The result is negative.")
+else:
+    print("The result is positive and negative")
