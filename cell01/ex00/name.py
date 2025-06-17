710e2a64c9d458842e58c54870ee161d7bfd1edd
psamphao <68030153@kmitl.ac.th>
2025-06-17 04:47:27 +0000

    cell00/ex1

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Paphawee"
+last_name = "samphao"
+print(first_name,last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,4 @@
+first_name = "Paphawee"
+last_name = "Samphao"
+whole_name = first_name + " " + last_name
+print(whole_name)
